74d1d32fa7d63c138fa0dedd127403eecb63da0c
gruu123 <61630485+gruu123@users.noreply.github.com>
2020-03-01 01:32:07 +0800

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,1 +1,9 @@
-alert('test');
+$(document.body).ready(function(){
+  
+    $(document.body).on('click', '#add_button', function(){
+      e.preventDefault();
+      alert('this is not working');
+    });
+  
+  
+});
